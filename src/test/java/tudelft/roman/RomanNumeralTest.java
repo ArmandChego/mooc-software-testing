a53f44c2c65c8b28ab55e64debcf42a532dfe2b2
Armando Manchego <armando.manchego@ucsm.edu.pe>
2025-03-19 15:04:57 -0500

    docs(roman): agregar comentarios sobre pruebas
    Se añadieron comentarios en RomanNumeralTest para explicar errores en cómo se interpretan ciertos casos incorrectos. No se modificó la implementación, solo documentación.

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -10,27 +10,27 @@ public class RomanNumeralTest {
     public void singleNumber() {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
+        Assertions.assertEquals(1, result); //Funciona correctamente
     }
 
     @Test
     public void numberWithManyDigits() {
         RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
+        int result = roman.convert("VIIII"); // Lo correcto seria IX para representar el 9
+        Assertions.assertEquals(9, result); //Aunque de igual forma con "VIIII" lo toma como un 9, lo cual es incorrecto
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
         RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("IV");
-        Assertions.assertEquals(4, result);
+        int result = roman.convert("IIX"); // Cuando se agrega un "I" extra solo considera el numero romano posterior al "I"
+        Assertions.assertEquals(10, result); //Se retorna 10 ya que se anulan los "II" al haber mas de 1, tomando solo en cuenta el "X"
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
         RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        int result = roman.convert("VIIIIVI"); // En este caso para el primer numero solo considera correctamente "VIII"
+        Assertions.assertEquals(13, result); // Para el segundo numero resta el "I" anterior al "V" y luego suma de nuevo "I"
     }
 }
